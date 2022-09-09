d9c7fd40031a379ef8b7ec94a8775061454dcd81
Gustavo Morini <gustavommh@gmail.com>
2022-09-09 11:32:13 -0300

    install and configure initial jwt-auth

--- config/app.php
+++ config/app.php
@@ -174,6 +174,10 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        /**
+         * jwt-auth
+         */
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
     ],
 
