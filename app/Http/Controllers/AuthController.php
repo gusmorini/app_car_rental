5724b28994cd522b699c74b3f539f6501edd17ff
Gustavo Morini <gustavommh@gmail.com>
2022-09-09 13:03:08 -0300

    auth controller and method login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        $credentials = $request->all(['email', 'password']);
+        $token = auth()->attempt($credentials);
+        if (!$token) return response()->json(['error' => 'invalid email or password'], 403);
+        return response()->json(['token' => $token], 200);
+    }
+
+    public function me()
+    {
+        return 'me';
+    }
+
+    public function logout()
+    {
+        return 'logout';
+    }
+
+    public function refresh()
+    {
+        return 'refresh';
+    }
+}
