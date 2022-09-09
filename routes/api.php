523b84354abb096a09ab55f73457e00b405800ea
Gustavo Morini <gustavommh@gmail.com>
2022-09-09 12:34:06 -0300

    config model and routes auth

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,11 @@ Route::apiResource('location', 'App\Http\Controllers\LocationController');
 Route::apiResource('brand', 'App\Http\Controllers\CarBrandController');
 Route::apiResource('model', 'App\Http\Controllers\CarModelController');
 Route::apiResource('car', 'App\Http\Controllers\CarController');
+
+/**
+ * auth-routes
+ */
+Route::post('login', 'App\Http\Controllers\AuthController@login');
+Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+Route::post('me', 'App\Http\Controllers\AuthController@me');
