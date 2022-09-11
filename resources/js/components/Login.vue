5c4e4b46f2feb529151e9b4d6a52ce5b38490599
Gustavo Morini <gustavommh@gmail.com>
2022-09-11 09:22:05 -0300

    component vue Login

---
+++ resources/js/components/Login.vue
@@ -0,0 +1,106 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">login (vue component)</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="">
+                            <div class="row mb-3">
+                                <label
+                                    for="email"
+                                    class="col-md-4 col-form-label text-md-end"
+                                    >E-mail</label
+                                >
+
+                                <div class="col-md-6">
+                                    <input
+                                        id="email"
+                                        type="email"
+                                        class="form-control"
+                                        name="email"
+                                        value=""
+                                        required
+                                        autocomplete="email"
+                                        autofocus
+                                    />
+
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>teste</strong>
+                                    </span>
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label
+                                    for="password"
+                                    class="col-md-4 col-form-label text-md-end"
+                                    >Senha</label
+                                >
+
+                                <div class="col-md-6">
+                                    <input
+                                        id="password"
+                                        type="password"
+                                        class="form-control"
+                                        name="password"
+                                        required
+                                        autocomplete="current-password"
+                                    />
+
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>teste</strong>
+                                    </span>
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <div class="col-md-6 offset-md-4">
+                                    <div class="form-check">
+                                        <input
+                                            class="form-check-input"
+                                            type="checkbox"
+                                            name="remember"
+                                            id="remember"
+                                        />
+
+                                        <label
+                                            class="form-check-label"
+                                            for="remember"
+                                        >
+                                            Relembre-me
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button
+                                        type="submit"
+                                        class="btn btn-primary"
+                                    >
+                                        Login
+                                    </button>
+
+                                    <a class="btn btn-link" href="">
+                                        esqueci a senha
+                                    </a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("Component login mounted.");
+    },
+};
+</script>
