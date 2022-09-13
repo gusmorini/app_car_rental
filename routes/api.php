ef06ac869d701781b16916248fb02d704b64b185
Gustavo Morini <gustavommh@gmail.com>
2022-09-13 14:17:15 -0300

    fix route /login v1 prefix

--- routes/api.php
+++ routes/api.php
@@ -18,18 +18,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1')->middleware('jwt.auth')->group(function(){
-    Route::apiResource('client', 'App\Http\Controllers\ClientController');
-    Route::apiResource('location', 'App\Http\Controllers\LocationController');
-    Route::apiResource('brand', 'App\Http\Controllers\CarBrandController');
-    Route::apiResource('model', 'App\Http\Controllers\CarModelController');
-    Route::apiResource('car', 'App\Http\Controllers\CarController');
-    // auth-routes
-    Route::post('me', 'App\Http\Controllers\AuthController@me');
-    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
-    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+Route::prefix('v1')->group(function(){
+    // routes protects
+    Route::middleware('jwt.auth')->group(function(){
+        Route::apiResource('client', 'App\Http\Controllers\ClientController');
+        Route::apiResource('location', 'App\Http\Controllers\LocationController');
+        Route::apiResource('brand', 'App\Http\Controllers\CarBrandController');
+        Route::apiResource('model', 'App\Http\Controllers\CarModelController');
+        Route::apiResource('car', 'App\Http\Controllers\CarController');
+        // auth-routes
+        Route::post('me', 'App\Http\Controllers\AuthController@me');
+        Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+        Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    });
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
 });
-/**
- * auth-routes
- */
-Route::post('login', 'App\Http\Controllers\AuthController@login');
