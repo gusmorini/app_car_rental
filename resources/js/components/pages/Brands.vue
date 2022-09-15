35b4b29f32e3d0ebe48b8c5f4621523a82cbacad
Gustavo Morini <gustavommh@gmail.com>
2022-09-15 13:49:11 -0300

    ajust path

--- resources/js/components/pages/Brands.vue
+++ resources/js/components/pages/Brands.vue
@@ -6,23 +6,44 @@
                     <template v-slot:body>
                         <div class="row">
                             <div class="mb-3 col-sm">
-                                <input-container-component id="brand-id" title="id da marca"
-                                    helptext="opcional, informe o id da marca">
-                                    <input type="number" class="form-control" id="brand-id"
-                                        aria-describedby="brand-id-help" placeholder="id da marca" />
+                                <input-container-component
+                                    id="brand-id"
+                                    title="id da marca"
+                                    helptext="opcional, informe o id da marca"
+                                >
+                                    <input
+                                        type="number"
+                                        class="form-control"
+                                        id="brand-id"
+                                        aria-describedby="brand-id-help"
+                                        placeholder="id da marca"
+                                    />
                                 </input-container-component>
                             </div>
                             <div class="mb-3 col-sm">
-                                <input-container-component id="brand-name" title="nome da marca"
-                                    helptext="opcional, informe o nome da marca">
-                                    <input type="text" class="form-control" id="brand-name"
-                                        aria-describedby="brand-name-help" placeholder="nome da marca" />
+                                <input-container-component
+                                    id="brand-name"
+                                    title="nome da marca"
+                                    helptext="opcional, informe o nome da marca"
+                                >
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        id="brand-name"
+                                        aria-describedby="brand-name-help"
+                                        placeholder="nome da marca"
+                                    />
                                 </input-container-component>
                             </div>
                         </div>
                     </template>
                     <template v-slot:footer>
-                        <button type="submit" class="btn btn-primary btn-sm float-end">Pesquisar</button>
+                        <button
+                            type="submit"
+                            class="btn btn-primary btn-sm float-end"
+                        >
+                            Pesquisar
+                        </button>
                     </template>
                 </card-component>
 
@@ -34,12 +55,27 @@
                                 <template>
                                     <tr v-for="key in brands">
                                         <th scope="row">{{ key.id }}</th>
-                                        <td class="text-uppercase">{{ key.name }}</td>
-                                        <td><img :src="'http://localhost:8000/storage/'+key.image" width="30" /></td>
+                                        <td class="text-uppercase">
+                                            {{ key.name }}
+                                        </td>
+                                        <td>
+                                            <img
+                                                :src="'/storage/' + key.image"
+                                                width="30"
+                                            />
+                                        </td>
                                         <td width="140">
-                                            <a class="btn btn-sm btn-light" href="#">editar</a>
-                                            <button class="btn btn-sm btn-light"
-                                                @click="brandDestroy(key.id)">deletar</button>
+                                            <a
+                                                class="btn btn-sm btn-light"
+                                                href="#"
+                                                >editar</a
+                                            >
+                                            <button
+                                                class="btn btn-sm btn-light"
+                                                @click="brandDestroy(key.id)"
+                                            >
+                                                deletar
+                                            </button>
                                         </td>
                                     </tr>
                                 </template>
@@ -49,12 +85,20 @@
                             <div>nenhum registro encontrado</div>
                         </template>
 
-                        <alerts-component :data="alert_destroy" v-if="alert_destroy.message" />
-
+                        <alerts-component
+                            :data="alert_destroy"
+                            v-if="alert_destroy.message"
+                        />
                     </template>
                     <template v-slot:footer>
-                        <button type="submit" class="btn btn-primary btn-sm float-end" data-bs-toggle="modal"
-                            data-bs-target="#addBrand">Adicionar</button>
+                        <button
+                            type="submit"
+                            class="btn btn-primary btn-sm float-end"
+                            data-bs-toggle="modal"
+                            data-bs-target="#addBrand"
+                        >
+                            Adicionar
+                        </button>
                     </template>
                 </card-component>
             </div>
@@ -62,57 +106,92 @@
         <!-- modal -->
         <modal-component id="addBrand" title="Adicionar Marca">
             <template v-slot:body>
-                <div class="d-flex flex-column flex-sm-row justify-content-center ">
-                    <label for="brand-image" class="align-items-center align-self-center p-2 border rounded me-4">
-                        <img src="https://evidenceencadernacao.com.br/wp-content/themes/claue/assets/images/placeholder.png"
-                            class="img-fluid btn" alt="logo" id="brand-logo" width="100" />
-                        <input class="form-control d-none" type="file" id="brand-image" @change="readImage($event)">
+                <div
+                    class="d-flex flex-column flex-sm-row justify-content-center"
+                >
+                    <label
+                        for="brand-image"
+                        class="align-items-center align-self-center p-2 border rounded me-4"
+                    >
+                        <img
+                            src="https://evidenceencadernacao.com.br/wp-content/themes/claue/assets/images/placeholder.png"
+                            class="img-fluid btn"
+                            alt="logo"
+                            id="brand-logo"
+                            width="100"
+                        />
+                        <input
+                            class="form-control d-none"
+                            type="file"
+                            id="brand-image"
+                            @change="readImage($event)"
+                        />
                     </label>
-                    <input-container-component id="add-brand-name" title="nome da marca"
-                        helptext="informe o nome da marca" class="flex-grow-1">
-                        <input type="text" class="form-control" id="add-brand-name"
-                            aria-describedby="add-brand-name-help" placeholder="nome da nova marca"
-                            v-model="brandName" />
+                    <input-container-component
+                        id="add-brand-name"
+                        title="nome da marca"
+                        helptext="informe o nome da marca"
+                        class="flex-grow-1"
+                    >
+                        <input
+                            type="text"
+                            class="form-control"
+                            id="add-brand-name"
+                            aria-describedby="add-brand-name-help"
+                            placeholder="nome da nova marca"
+                            v-model="brandName"
+                        />
                     </input-container-component>
                 </div>
-
             </template>
             <template v-slot:alerts v-if="alert_save.message" class="mt-4">
                 <alerts-component :data="alert_save" />
             </template>
             <template v-slot:footer>
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                <button type="button" class="btn btn-primary" @click="brandSave()">Salvar</button>
+                <button
+                    type="button"
+                    class="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                >
+                    Fechar
+                </button>
+                <button
+                    type="button"
+                    class="btn btn-primary"
+                    @click="brandSave()"
+                >
+                    Salvar
+                </button>
             </template>
         </modal-component>
     </div>
 </template>
 
 <script>
-import api from '../../../../services/api';
+import api from "../../../../services/api";
 
 export default {
     data() {
         return {
-            brandName: '',
+            brandName: "",
             brandImage: [],
             brands: [],
             type: null,
-            text: '',
+            text: "",
             alert_destroy: {
                 type: null,
-                message: ''
+                message: "",
             },
             alert_save: {
                 type: null,
-                message: '',
-            }
-        }
+                message: "",
+            },
+        };
     },
     mounted() {
-        api.get('/brand')
-            .then(({ data }) => this.brands = data)
-            .catch(e => console.log(e))
+        api.get("/brand")
+            .then(({ data }) => (this.brands = data))
+            .catch((e) => console.log(e));
     },
     methods: {
         loadImage(e) {
@@ -121,7 +200,7 @@ export default {
         },
         readImage({ target }) {
             if (target.files && target.files[0]) {
-                console.log('rodando')
+                console.log("rodando");
                 var file = new FileReader();
                 file.onload = function (e) {
                     document.getElementById("brand-logo").src = e.target.result;
@@ -132,45 +211,46 @@ export default {
         },
         brandSave() {
             let formData = new FormData();
-            formData.append('name', this.brandName);
-            formData.append('image', this.brandImage);
+            formData.append("name", this.brandName);
+            formData.append("image", this.brandImage);
 
-            api.post('/brand', formData, { headers: { 'Content-Type': 'multipart/form-data' } })
-                .then(res => {
+            api.post("/brand", formData, {
+                headers: { "Content-Type": "multipart/form-data" },
+            })
+                .then((res) => {
                     this.alert_save = {
-                        type: 'success',
-                        message: 'a marca foi registrada'
-                    }
+                        type: "success",
+                        message: "a marca foi registrada",
+                    };
                     this.brands.push(res.data);
                 })
-                .catch(e => {
+                .catch((e) => {
                     this.alert_save = {
-                        type: 'danger',
-                        message: e.response.data.errors
-                    }
-                    console.log(e.response.data.errors)
-                })
+                        type: "danger",
+                        message: e.response.data.errors,
+                    };
+                    console.log(e.response.data.errors);
+                });
         },
         brandDestroy(id) {
-
-            if (!confirm('Realmente apagar o regitro?')) return;
+            if (!confirm("Realmente apagar o regitro?")) return;
 
             api.delete(`/brand/${id}`)
-                .then(res => {
+                .then((res) => {
                     this.alert_destroy = {
-                        type: 'success',
-                        message: 'item foi deletado'
-                    }
-                    this.brands = this.brands.filter(e => e.id != id);
+                        type: "success",
+                        message: "item foi deletado",
+                    };
+                    this.brands = this.brands.filter((e) => e.id != id);
                 })
-                .catch(e => {
+                .catch((e) => {
                     this.alert_destroy = {
-                        type: 'danger',
-                        message: 'erro ao deletar registro'
-                    }
-                    console.log(e.response)
-                })
-        }
-    }
-}
+                        type: "danger",
+                        message: "erro ao deletar registro",
+                    };
+                    console.log(e.response);
+                });
+        },
+    },
+};
 </script>
