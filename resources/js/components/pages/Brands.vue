2741626e551b3ce1cf0089a16bb4d68cf457305c
Gustavo Morini <gustavommh@gmail.com>
2022-09-20 15:23:54 -0300

    ajus components brands

--- resources/js/components/pages/Brands.vue
+++ resources/js/components/pages/Brands.vue
@@ -4,27 +4,15 @@
             <div class="col-md-12">
                 <card-component title="Buscar de marcas">
                     <template v-slot:body>
-                        <div class="row">
-                            <div class="mb-3 col-sm">
-                                <input-container-component id="brand-id" title="id da marca"
-                                    helptext="opcional, informe o id da marca">
-                                    <input type="number" class="form-control" id="brand-id"
-                                        aria-describedby="brand-id-help" placeholder="id da marca"
-                                        v-model="search.id" />
-                                </input-container-component>
-                            </div>
-                            <div class="mb-3 col-sm">
-                                <input-container-component id="brand-name" title="nome da marca"
-                                    helptext="opcional, informe o nome da marca">
-                                    <input type="text" class="form-control" id="brand-name"
-                                        aria-describedby="brand-name-help" placeholder="nome da marca"
-                                        v-model="search.name" />
-                                </input-container-component>
-                            </div>
-                        </div>
+                        <input-container-component id="brand-name" title="marca"
+                            helptext="opcional, informe o nome da marca">
+                            <input type="text" class="form-control" id="brand-name" aria-describedby="brand-name-help"
+                                placeholder="nome da marca" v-model="search" />
+                        </input-container-component>
                     </template>
                     <template v-slot:footer>
-                        <button type="submit" class="btn btn-primary btn-sm float-end" @click="findBrand()">
+                        <button type="submit" class="btn btn-outline-primary btn-sm float-end" @click="findBrand()">
+                            <i class="bi bi-search"></i>
                             Pesquisar
                         </button>
                     </template>
@@ -55,8 +43,8 @@
                                         </td>
                                         <td width="120">
                                             <a class="btn btn-sm btn-light" @click="setItem(brand)" href="#"
-                                                data-bs-toggle="modal" data-bs-target="#brand-modal"><i
-                                                    class="bi bi-eye"></i>
+                                                data-bs-toggle="modal" data-bs-target="#brand-modal">
+                                                <i class="bi bi-eye"></i>
                                                 visualizar
                                             </a>
                                         </td>
@@ -96,6 +84,7 @@
         <modal-component id="brand-modal" :title="selected.title">
             <template v-slot:body>
                 <div class="d-flex flex-column justify-content-center">
+
                     <div class="align-items-center align-self-center p-2 border rounded">
                         <img :src="selected.image ? 'storage/'+selected.image : '/storage/image.png'" class="img-fluid"
                             alt="logo" id="brand-logo" width="120" />
@@ -105,35 +94,32 @@
                         helptext="informe o nome da marca" class="flex-grow-1">
                         <input type="text" class="form-control" id="add-brand-name"
                             aria-describedby="add-brand-name-help" placeholder="nome da nova marca"
-                            v-model="selected.name" :disabled="!selected.edit" />
+                            v-model="selected.name" />
                     </input-container-component>
+
                     <input-container-component id="add-brand-image" title="logo da marca"
                         helptext="informe o logo da marca" class="flex-grow-1">
-                        <input class="form-control" type="file" id="brand-image" @change="readImage($event)"
-                            :disabled="!selected.edit" />
+                        <input class="form-control" type="file" id="brand-image" @change="readImage($event)" />
                     </input-container-component>
+
                 </div>
             </template>
 
-            <template v-slot:alerts class="mt-4" v-if="alert.type">
-                <alerts-component :data="alert" />
+            <template v-slot:alerts class="mt-4" v-if="selected.feedback">
+                <alerts-component :message="selected.feedback.message" :title="selected.feedback.title"
+                    :type="selected.feedback.type" />
             </template>
 
             <template v-slot:footer>
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-                    Fechar
+                <button v-if="selected.id" type="button" class="btn btn-outline-danger"
+                    @click="brandDestroy(selected.id)">
+                    <i class="bi bi-trash"></i>
+                    apagar
                 </button>
-                <button type="button" class="btn btn-primary" v-if="!selected.edit" @click="selected.edit = true">
-                    editar
+                <button type="button" class="btn btn-outline-primary" @click="brandSave()">
+                    <i class="bi bi-check-all"></i>
+                    {{ selected.id ? 'atualizar' : 'salvar' }}
                 </button>
-                <template v-else>
-                    <button v-if="selected.id" type="button" class="btn btn-danger" @click="brandDestroy(selected.id)">
-                        apagar
-                    </button>
-                    <button type="button" class="btn btn-success" @click="brandSave()">
-                        {{ selected.id ? 'atualizar' : 'salvar' }}
-                    </button>
-                </template>
             </template>
 
         </modal-component>
@@ -142,8 +128,7 @@
 </template>
 
 <script>
-import api from "../../../../services/api";
-import { Modal } from 'bootstrap';
+// import api from "../../../../services/api";
 
 export default {
     data() {
@@ -151,13 +136,13 @@ export default {
             brands: [],
             links: [],
             page: 1,
-            search: {
-                id: '',
-                name: '',
-            },
+            search: '',
         };
     },
     computed: {
+        api() {
+            return this.$store.state.api;
+        },
         selected() {
             return this.$store.state.selected;
         },
@@ -172,7 +157,6 @@ export default {
         setItem(item) {
             if (!item) return this.$store.state.selected = { name: '', image: '', title: 'adicionar marca', edit: true }
             item.title = 'visualizar marca';
-            item.edit = false;
             this.$store.state.selected = { ...this.$store.state.selected, ...item };
         },
         readImage({ target }) {
@@ -186,20 +170,16 @@ export default {
             this.$store.state.selected.image_file = target.files[0];
         },
         findBrand() {
-            const s = this.search;
             let params = '';
-            for (let key in s) {
-                if (s[key]) {
-                    if (params !== '') params += ';';
-                    params += `${key},LIKE,${s[key]}%`;
-                }
+            if (this.search) {
+                params = `name,LIKE,${this.search}%`;
             }
             params ? this.getBrands(null, params) : this.getBrands();
         },
         getBrands(page = this.page, search = '') {
             let url = `/brand?page=${page ? page : 1}`;
             search ? url += `&search=${search}` : false;
-            api.get(url)
+            this.api.get(url)
                 .then(({ data }) => {
                     this.brands = data.data;
                     this.links = data.links;
@@ -224,21 +204,40 @@ export default {
                 formData.append("_method", "patch");
             }
 
-            api.post(url, formData, {
+            this.api.post(url, formData, {
                 headers: { "Content-Type": "multipart/form-data" },
             })
                 .then((res) => {
-                    this.toast({ title: 'sucesso', message: `registro ID ${res.data.id} foi salvo` })
+                    let temp = {
+                        ...this.selected,
+                        image_file: '',
+                        feedback: {
+                            title: 'registro adicionado',
+                            message: `registro ID ${res.data.id} adicionado com sucesso`,
+                        }
+                    }
+                    this.setItem(temp);
                     this.getBrands()
                 })
                 .catch((e) => {
-                    console.log(e.response.data.errors);
+                    console.log(e);
+                    const temp = {
+                        ...this.selected,
+                        edit: true,
+                        feedback: {
+                            type: 'danger',
+                            title: 'erro ao adicionar registro',
+                            message: '...'
+                        }
+                    };
+                    console.log(temp)
+                    this.setItem(temp)
                 });
         },
         brandDestroy(id) {
             if (!confirm("Realmente apagar o regitro?")) return;
 
-            api.delete(`/brand/${id}`)
+            this.api.delete(`/brand/${id}`)
                 .then((res) => {
                     this.toast({ title: 'sucesso', message: 'o item foi deletado' })
                     this.getBrands();
