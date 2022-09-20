4f117b3110eca224bfca3069d8809ea267234028
Gustavo Morini <gustavommh@gmail.com>
2022-09-20 15:22:08 -0300

    add api in store

--- resources/js/store.js
+++ resources/js/store.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import { toast } from "./toast";
+import api from "../../services/api";
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
+        api,
         selected: {},
         alert: {},
         toast,
